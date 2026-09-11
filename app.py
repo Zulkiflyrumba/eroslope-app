487de37dbca0dcf008bc743e436b9b1cdc0eeea1
Zulkiflyrumba <76906191+Zulkiflyrumba@users.noreply.github.com>
2026-09-11 09:19:00 +0800

    app.py 02

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from shapely.validation import make_valid
 from shapely import vectorized
 from shapely.ops import linemerge, polygonize, unary_union
 import os
-
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import plotly.graph_objects as go
